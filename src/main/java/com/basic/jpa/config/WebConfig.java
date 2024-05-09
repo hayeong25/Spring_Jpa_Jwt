2b7bd7bf4a21b3fc8ee9307916503e2dbca1367d
hayeong25 <hayeong0624@gmail.com>
2024-05-09 15:56:12 +0900

    feat: @Configuration 추가

--- src/main/java/com/basic/jpa/config/WebConfig.java
+++ src/main/java/com/basic/jpa/config/WebConfig.java
@@ -1,9 +1,11 @@
 package com.basic.jpa.config;
 
 import com.basic.jpa.interceptor.AuthenticationInterceptor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
